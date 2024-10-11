64097ed063d95963ed3ca29ba2708846146a5374
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-10 22:25:26 -0300

    first commit

---
+++ Teste/UsuarioControllerTeste.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web_app_domain;
+using web_app_performance.Controllers;
+using web_app_repository;
+
+namespace Teste
+{
+    public class UsuarioControllerTeste
+    {
+
+        private readonly Mock<IUsuarioRepository> _userRepositoryMock;
+        private readonly UsuarioController _controller;
+
+
+        public UsuarioControllerTeste()
+        {
+            _userRepositoryMock = new Mock<IUsuarioRepository>();
+            _controller = new UsuarioController(_userRepositoryMock.Object);
+
+
+        }
+
+
+        [Fact]
+        public async Task Get_ListarUsuariosOk()
+        {
+            var usuarios = new List<Usuario>()
+            {
+                new Usuario()
+                {
+                    Email = "xxx@gmail.com",
+                    Id = 1,
+                    Nome = "rick silva"
+                }
+            };
+            _userRepositoryMock.Setup(r => r.ListarUsuarios()).ReturnsAsync(usuarios);
+
+
+            //Act
+
+            var result = await _controller.GetUsuario();
+
+
+            //ASSERTS
+            Assert.IsType<OkObjectResult>(result);
+            var OkResult = result as OkObjectResult;
+            Assert.Equal(JsonConvert.SerializeObject(usuarios), JsonConvert.SerializeObject(OkResult.Value));
+
+
+
+
+
+
+
+        }
+
+
+
+        [Fact]
+        public async Task Get_ListarRetornarNotFound()
+        {
+            _userRepositoryMock.Setup(u => u.ListarUsuarios()).ReturnsAsync((IEnumerable<Usuario>)null);
+
+            var result = await _controller.GetUsuario();
+            Assert.IsType<NotFoundResult>(result);
+
+        }
+        [Fact]
+        public async Task Post_SalvarUsuario()
+        {
+            var usuario = new Usuario()
+            {
+
+                Id = 1,
+                Email = "xxx@gmail.com",
+                Nome = "ricky silva"
+
+            };
+            _userRepositoryMock.Setup(u => u.SalvarUsuario(It.IsAny<Usuario>())).Returns(Task.CompletedTask);
+
+            var result = await  _controller.Post(usuario);
+
+            //Assert.IsType<OkObjectResult>(result);
+            _userRepositoryMock.Verify(u => u.SalvarUsuario(It.IsAny<Usuario>()), Times.Once);
+
+        }
+    }
+}
---
+++ Teste/UsuarioRepositoryTeste.cs
@@ -0,0 +1,71 @@
+﻿
+
+using Moq;
+using web_app_domain;
+using web_app_repository;
+
+namespace Teste
+{
+    public class UsuarioRepositoryTeste
+    {
+        [Fact]
+
+        public async Task ListarUsuarios()
+        {
+            var usuarios = new List<Usuario>()
+            {
+                  new Usuario()
+            {
+                Email = "xxx@gmail.com",
+                Id = 1,
+                Nome = "rick silva"
+            },
+            new Usuario()
+
+            {
+                Email = "xxx@gmail.com",
+                Id = 2,
+                Nome = "rick silva"
+
+            }
+            };
+
+          
+             var userRepositoryMock = new Mock<IUsuarioRepository>();
+            userRepositoryMock.Setup(u => u.ListarUsuarios()).ReturnsAsync(usuarios);
+            var userRepository = userRepositoryMock.Object;
+
+           var result = await userRepository.ListarUsuarios();
+
+            Assert.Equal(usuarios , result);
+
+        }
+
+     
+
+        [Fact]
+        public async Task SalvarUsuario()
+        {
+            var usuario = new Usuario()
+            {
+
+                Id = 1,
+                Email = "xxx@gmail.com",
+                Nome = "ricky silva"
+
+            };
+
+            var userRepositoryMock = new Mock<IUsuarioRepository>();
+            userRepositoryMock
+                .Setup(u => u.SalvarUsuario(It.IsAny<Usuario>()))
+                .Returns(Task.CompletedTask);
+            var userRepository = userRepositoryMock.Object;
+
+            await userRepository.SalvarUsuario(usuario);
+
+            userRepositoryMock.Verify(u=>u.SalvarUsuario(It.IsAny<Usuario>()), Times.Once);
+        }
+
+    }
+}
+
